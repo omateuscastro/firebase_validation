0f3bd44ed012deac3bd6dd8e530c214f03cdd05f
Vinícius Boscardin <boscardinvinicius@gmail.com>
2020-05-22 09:38:54 -0300

    refactor all firebase_validation

---
+++ lib/model/firebase.dart
@@ -0,0 +1,35 @@
+class Firebase {
+  String ativo;
+  String blackList;
+  int codCli;
+  int diasAutenticacao;
+  String dtValidade;
+  String enderecoPacific;
+  String nomeCli;
+  int versaoMinima;
+
+  Firebase(
+      {this.ativo,
+      this.blackList,
+      this.codCli,
+      this.diasAutenticacao,
+      this.dtValidade,
+      this.enderecoPacific,
+      this.nomeCli,
+      this.versaoMinima});
+
+  factory Firebase.fromJson(Map<String, dynamic> json) => Firebase(
+        ativo: json["ativo"] == null ? null : json["ativo"],
+        blackList: json["black_list"] == null ? null : json["black_list"],
+        codCli: json["cod_cli"] == null ? null : json["cod_cli"],
+        diasAutenticacao: json["dias_autenticacao"] == null
+            ? null
+            : json["dias_autenticacao"],
+        dtValidade: json["dt_validade"] == null ? null : json["dt_validade"],
+        enderecoPacific:
+            json["endereco_pacific"] == null ? null : json["endereco_pacific"],
+        nomeCli: json["nome_cli"] == null ? null : json["nome_cli"],
+        versaoMinima:
+            json["versao_minima"] == null ? null : json["versao_minima"],
+      );
+}
